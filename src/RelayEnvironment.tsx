17f183fd333e22061fbb27847be038c21df5f479
Steve Hartken <steve.hartken@gmail.com>
2020-10-21 23:03:09 -0400

    #1 -- Update Demo RelayEnvironment
    Update demo relay enviromnent with new args for fetchPeer and state management

--- src/RelayEnvironment.tsx
+++ src/RelayEnvironment.tsx
@@ -1,9 +1,13 @@
 import { Environment, Network, RecordSource, Store } from 'relay-runtime'
-import { fetchPeer } from './index'
+import { fetchPeer, manage } from './index'
+import { schema, root } from './graphql/resolve'
 
 const environment = new Environment({
-  network: Network.create(fetchPeer),
+  network: Network.create(fetchPeer(schema, root)),
   store: new Store(new RecordSource())
 })
+
+// pass environment to eventEmitter for state management
+manage(environment)
 
 export default environment
