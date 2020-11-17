81e34494ecdcf2e21f34b8579d275a5748444bca
Steve Hartken <steve.hartken@gmail.com>
2020-11-17 11:32:29 -0500

    #6 -- codegen runtime
    Needed object validation, not union.
    
    Added default value for undefined types in codegen. undefined in
    io-ts means something similar to null

--- src/graphql/codegen.runtime.js
+++ src/graphql/codegen.runtime.js
@@ -2,6 +2,40 @@ const { loadSchema } = require('@graphql-tools/load');
 const { GraphQLFileLoader } = require('@graphql-tools/graphql-file-loader');
 const { mergeSchemas } = require('@graphql-tools/merge');
 
+const undefinedDefault = 'string';
+
+const dict = {
+    'String': 'string',
+    'Int': 'number',
+    'Float': 'number',
+    'Boolean': 'boolean',
+    'id': 'string',
+}
+
+const lookup = (field) => {
+    if (dict.hasOwnProperty(field.type.name.value)) {
+        return `${field.name.value}: t.${dict[field.type.name.value]}`
+    }
+    else {
+        return `${field.name.value}: t.${undefinedDefault}`
+    }
+}
+
+
+const format = (fields) => fields.map(lookup).join(`,
+      `)
+
+
+const template = ({ acc, name, fields }) =>
+    `${acc}
+  t.record(
+    t.literal('${name}'),
+    t.type({
+      ${format(fields)}
+    })
+  ),`
+
+
 module.exports = {
     plugin: async (schema, documents, config) => {
 
@@ -14,23 +48,16 @@ module.exports = {
 
         const typesMap = merged.getTypeMap();
 
-        const dict = {
-            'String': 'string',
-            'Int': 'number',
-            'Float': 'number',
-            'Boolean': 'boolean',
-            'id': 'string',
-        }
         const reduce = typesMap.Query.astNode.fields.reduce((acc, field) => {
+            const name = field.name.value;
             if (field.type.hasOwnProperty('type')) {
+                const fields = typesMap[field.type.type.name.value].astNode.fields;
                 // custom graphql type
-                return `${acc}\n  t.record(\n    t.literal('${field.name.value}'),\n    t.union([\n      ${typesMap[field.type.type.name.value]
-                    .astNode.fields.map((field) =>
-                        `t.record(t.literal('${field.name.value}'), t.${dict[field.type.name.value]})`
-                    ).join(',\n      ')}\n    ])\n  ),`
+                return template({ acc, name, fields })
             }
-            //scalar values only
-            return `${acc}\n  t.record(t.literal('${field.name.value}'), t.${dict[field.type.name.value]}),`;
+            const value = field.type.name.value
+            //scalar type
+            return `${acc}\n  t.record(t.literal('${name}'), t.${dict[value]}),`;
         }, '');
 
         return `import * as t from 'io-ts'\n\nexport const Query = t.union([${reduce}\n])`
